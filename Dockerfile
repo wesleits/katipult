24cb88dc93f1d34bce9e2b9679484bee563d2226
DESKTOP-AJLHFBP\Enterprise <wesleiteixeira@hotmail.com.br>
2017-07-06 19:46:32 -0300

    ok

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,5 @@ RUN apt-get update && \
 	apt-get install -y nodejs
 
 COPY ./nodejs /var/nodejs/
+
+CMD nodejs /var/nodejs/main.js
--- nodejs/main.js
+++ nodejs/main.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) 
 {
 	res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
-  	res.write('Olá Mundo! Esse é o site SITEADDRESS');
+  	res.write('Olá Mundo!');
 	res.end();
 }) 
 .listen(80);
