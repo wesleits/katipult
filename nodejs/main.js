d18b3ae72c19e87c3a9b08a7b9fbbf6700388bb0
DESKTOP-AJLHFBP\Enterprise <wesleiteixeira@hotmail.com.br>
2017-07-06 18:19:39 -0300

    ok

--- nodejs/main.js
+++ nodejs/main.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) 
 {
 	res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8'});
-  	res.write('Olá Mundo! Essa é a v9');
+  	res.write('Olá Mundo! Esse é o site SITEADDRESS');
 	res.end();
 }) 
 .listen(80);
